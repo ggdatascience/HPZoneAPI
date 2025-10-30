81ed53f0e4b7ed80f9c2256cef01091f0aecf54d
Aart Dijkstra <a.dijkstra@ggdnog.nl>
2025-10-30 15:21:43 +0100

    Accidentally swapped the argument order in the |> rewrite.

--- R/HPZone_convert_dates.R
+++ R/HPZone_convert_dates.R
@@ -27,7 +27,7 @@ HPZone_convert_dates = function (data, search="dat(e|um)|Received_on", statdate=
     dataset = data$items
   }
 
-  cols = stringr::str_detect(which(colnames(dataset), stringr::regex(search, ignore_case=T)))
+  cols = which(stringr::str_detect(colnames(dataset), stringr::regex(search, ignore_case=T)))
   for (col in cols) {
     dataset[[col]] = suppressWarnings(lubridate::ymd_hms(dataset[[col]]))
   }
